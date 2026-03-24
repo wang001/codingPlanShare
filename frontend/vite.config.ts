6989823317894eb7afc8dd95bc820828d8958f0a
wang001 <1617581104@qq.com>
2026-03-24 17:08:32 +0800

    feat: rewrite frontend - frontend/vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false
+      },
+      '/admin': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
