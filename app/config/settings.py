2fc15e4bb0b31c6645e4a360da389b081a0be540
wang001 <1617581104@qq.com>
2026-03-28 21:37:38 +0800

    feat(pricing+tokens): app/config/settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -69,6 +69,7 @@ class Settings:
         self.cache          = self.config.get('cache', {})
         self.logging        = self.config.get('logging', {})
         self.key_reward     = self.config.get('key_reward', {})
+        self.model_pricing  = self.config.get('model_pricing', {})
 
 
 settings = Settings()
